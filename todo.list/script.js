b51e1396b4fe58c2ce68ad064c86aedfaa7c1386
Ademx1 <adaman@code.edu.az>
2023-06-07 01:13:58 +0400

    first commit

---
+++ todo.list/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let list = [];
+let listUl = document.getElementById("list");
+
+function todo() {
+  let inp = document.getElementById("inp");
+
+  if (inp.value.trim() === "") {
+    Swal.fire('No data available. Please try again!')
+
+    return;
+  }
+
+  list.push(inp.value);
+  inp.value = "";
+  display();
+  
+}
+
+function Delete(ind) {
+  list.splice(ind, 1);
+    display();
+}
+
+function display() {
+  let data = "";
+
+  for (let i = 0; i < list.length; i++) {
+    data += ` <li class="list-group-item d-flex justify-content-between">${list[i]} <button onclick="Delete(${i})" class="btn btn-danger btn-sm">Delete</button> </li>`;
+  }
+
+  listUl.innerHTML = data;
+}
